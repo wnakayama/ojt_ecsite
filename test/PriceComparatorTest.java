6d767313b5f565141a426f11ea7a5a5b12942209
Wataru.Nakayama <Wataru.Nakayama@jp.ricoh.com>
2020-01-27 16:22:52 +0900

    インターフェース定義に違反していたcompareメソッドの期待結果を修正

--- test/PriceComparatorTest.java
+++ test/PriceComparatorTest.java
@@ -28,7 +28,7 @@ public class PriceComparatorTest {
     }
 
     /**
-     * 2つの商品データの税込み価格が同じであるとき,compareメソッドは1を返却する.
+     * 2つの商品データの税込み価格が同じであるとき,compareメソッドは0を返却する.
      */
     @Test
     public void evenPrice() {
@@ -39,9 +39,7 @@ public class PriceComparatorTest {
 
         int compareResult = priceComparator.compare(firstProduct, secondProduct);
 
-        // p1.getPriceIncludeTax() < p2.getPriceIncludeTax()) ? -1 : 1;
-        // 上記のようにcompareメソッドを実装するため,同じ価格であっても0ではなく1を返却することを期待する.
-        assertThat(compareResult, is(1));
+        assertThat(compareResult, is(0));
     }
 
     /**
