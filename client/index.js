b2f4f342e69dd01e149d4c71eba028ed906b0382
Wataru.Nakayama <Wataru.Nakayama@jp.ricoh.com>
2020-01-31 13:29:10 +0900

    実装: 4商品ずつ横並びにする, マウスオーバーで全文表示する

--- client/index.js
+++ client/index.js
@@ -17,21 +17,24 @@ function sendget() {
                 $('.productInfoArea').append('商品データがありません<br>');
             } else {
                 $('.productInfoArea').append('<table class="productInfoTable"></table>');
-                // (途中)<tr></tr>で新しい列を作成　4件ごとに改行するイメージで商品データを並べたい
-                for (var i = 0, len = product.length; i < len; i++) {
-                    if (i % 4 == 0) {
-                        $('.productInfoTable').append('<tr>');
-                    }
-                    $('.productInfoTable').append('<td class="product' + i + '"></td>');
-                    $('.product' + i + '').append('<li data-text="' + product[i].name + '"><h4>' + omitLongProductName(product[i].name) + '</h4>');
-                    $('.product' + i + '').append(separateWithComma(product[i].priceIncludeTax) + '円 ');
-                    $('.product' + i + '').append('(' + separateWithComma(product[i].priceExcludeTax) + '円) <br>');
-                    $('.product' + i + '').append('<input type="checkbox" name="choose" value="1">選択' + '<br></li>');
+                var j = 0;
+                const cols = 4;
+                const rows = (product.length) / cols;
 
-                    if (i % 4 == 0) {
-                        $('.productInfoTable').append('<tr>');
+                for (var i = 0; i < rows; i++) {
+
+                    $('.productInfoTable').append('<tr class ="test' + i + '">');
+
+                    for (var k = 0; k < cols; k++) {
+                        $('.test' + i + '').append('<td class="product' + j + '"></td>');
+                        $('.product' + j + '').append('<li data-text="' + product[j].name + '"><h4>' + omitLongProductName(product[j].name) + '</h4>');
+                        $('.product' + j + '').append(separateWithComma(product[j].priceIncludeTax) + '円 ');
+                        $('.product' + j + '').append('(' + separateWithComma(product[j].priceExcludeTax) + '円) <br>');
+                        $('.product' + j + '').append('<input type="checkbox" name="choose" value="1">選択' + '<br></li>');
+                        j++
                     }
                 }
+
             }
 
         }).fail(function (error) {
@@ -49,13 +52,12 @@ function omitLongProductName(name) {
     return name.length > 30 ? (name).slice(0, 30) + "…" : name;
 }
 
-// マウスオーバー商品名の吹き出し
-// $(".product li").on({
-// 	'mouseenter': function () {
-// 		var text = $(this).attr('data-text');
-// 		$(this).append('<div class=#product-tooltips">' + text + '</div>');
-// 	},
-// 	'mouseleave': function () {
-// 		$(this).find("#product-tooltips").remove();
-// 	}
-// });
+
+$(document).on(
+    'mouseover', 'li', function () {
+
+        var datatext = $(this).attr('data-text');
+        $(this).attr('title', datatext);
+        // $(this).append('<div class=#product-tooltips">' + text + '</div>');
+    }
+);
