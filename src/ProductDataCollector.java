cd05ed111ed70543c3bfa22925fa6feb3f9812c2
Wataru.Nakayama <Wataru.Nakayama@jp.ricoh.com>
2020-02-18 10:53:52 +0900

    リクエストのあった商品を全商品データから探し出すロジックを修正(List→Map)

--- src/ProductDataCollector.java
+++ src/ProductDataCollector.java
@@ -1,7 +1,10 @@
 package ojt_ecsite;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * リクエストのあった商品IDを手掛かりに,利用者が購入したがっている商品は何か,全商品データから探し出すクラス.
@@ -24,17 +27,15 @@ public class ProductDataCollector {
      * @return purchasedProductList 全商品データと照合が取れた選択済み商品のリスト
      */
     public static List<Product> collectProductData(String[] checkValues) {
+
+        Map<Integer, Product> allProductMap = new HashMap<>();
+        allProductMap = allProductList.stream().collect(
+                Collectors.toMap(allProductList -> allProductList.getProductID(), allProductList -> allProductList));
+
         List<Product> purchasedProductList = new ArrayList<>();
         for (int i = 0; i < checkValues.length; i++) {
             int requestedID = Integer.parseInt(checkValues[i]);
-
-            for (int j = 0; j < allProductList.size(); j++) {
-                int collationID = allProductList.get(j).getProductID();
-
-                if (requestedID == collationID) {
-                    purchasedProductList.add(allProductList.get(j));
-                }
-            }
+            purchasedProductList.add(allProductMap.get(requestedID));
         }
         return purchasedProductList;
     }
