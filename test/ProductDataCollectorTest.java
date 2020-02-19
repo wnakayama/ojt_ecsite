f6a438b32d14183114fac54b8ea4a1e97ec94778
Wataru.Nakayama <Wataru.Nakayama@jp.ricoh.com>
2020-02-19 13:11:34 +0900

    テストケース追加, 命名を一部修正

--- test/ProductDataCollectorTest.java
+++ test/ProductDataCollectorTest.java
@@ -77,7 +77,7 @@ public class ProductDataCollectorTest {
      * @param requestedProductID 利用者が選択した商品のID
      */
     @Test
-    public void allOfRequestProductIdAreWrong() {
+    public void allRequestProductIdAreWrong() {
         Product productForTest1 = new Product(1, "テスト1", "飲食料品", 100, 108, null);
         Product productForTest2 = new Product(2, "テスト2", "飲食料品", 200, 216, null);
         Product productForTest3 = new Product(3, "テスト3", "酒類", 300, 330, null);
@@ -96,14 +96,38 @@ public class ProductDataCollectorTest {
     }
 
     /**
-     * 商品のIDにnullが含まれているとき,ProductDataCollector単体ではNullPointerExceptionが発生する.
-     * (結合後は基本的に発生しないケース)
+     * リクエストしている商品のIDにnullが含まれているとき,
+     * ProductDataCollector単体ではNumberFormatExceptionが発生する. (結合後は基本的に発生しないケース)
+     *
+     * @param allProductList     全商品データのリスト
+     * @param requestedProductID 利用者が選択した商品のID
+     */
+    @Test(expected = NumberFormatException.class)
+    public void requestProductIdContainsNull() {
+        Product productForTest1 = new Product(1, "テスト1", "飲食料品", 100, 108, null);
+        Product productForTest2 = new Product(2, "テスト2", "飲食料品", 200, 216, null);
+        Product productForTest3 = new Product(3, "テスト3", "酒類", 300, 330, null);
+
+        List<Product> allProductList = new ArrayList<>();
+        allProductList.add(productForTest1);
+        allProductList.add(productForTest2);
+        allProductList.add(productForTest3);
+
+        String[] requestProductID = { "1", null, "3" };
+
+        ProductDataCollector.setAllProductList(allProductList);
+        List<Product> result = ProductDataCollector.collectProductData(requestProductID);
+    }
+
+    /**
+     * リクエストしている商品のIDを格納するString配列がnullのとき,
+     * ProductDataCollector単体ではNullPointerExceptionが発生する. (結合後は基本的に発生しないケース)
      *
      * @param allProductList     全商品データのリスト
      * @param requestedProductID 利用者が選択した商品のID
      */
     @Test(expected = NullPointerException.class)
-    public void requestProductIdIsNull() {
+    public void requestProductIdArrayIsNull() {
         Product productForTest1 = new Product(1, "テスト1", "飲食料品", 100, 108, null);
         Product productForTest2 = new Product(2, "テスト2", "飲食料品", 200, 216, null);
         Product productForTest3 = new Product(3, "テスト3", "酒類", 300, 330, null);
@@ -120,13 +144,39 @@ public class ProductDataCollectorTest {
     }
 
     /**
+     * リクエストしている商品のIDが重複している場合, 対応する商品が指定した順番で入ったList<Product>が返却される.
+     * (重複している分も,そのままListに格納して返却される.)
+     *
+     * @param allProductList     全商品データのリスト
+     * @param requestedProductID 利用者が選択した商品のID
+     */
+    @Test
+    public void requestDuplicatedProductId() {
+        Product productForTest1 = new Product(1, "テスト1", "飲食料品", 100, 108, null);
+        Product productForTest2 = new Product(2, "テスト2", "飲食料品", 200, 216, null);
+        Product productForTest3 = new Product(3, "テスト3", "酒類", 300, 330, null);
+
+        List<Product> allProductList = new ArrayList<>();
+        allProductList.add(productForTest1);
+        allProductList.add(productForTest2);
+        allProductList.add(productForTest3);
+
+        String[] requestProductID = { "1", "3", "3" };
+
+        ProductDataCollector.setAllProductList(allProductList);
+        List<Product> result = ProductDataCollector.collectProductData(requestProductID);
+        assertThat(result, is(contains(productForTest1, productForTest3, productForTest3)));
+        assertThat(result.size(), is(3));
+    }
+
+    /**
      * 全商品データのリストが空のとき,商品IDを指定していても照合は行われず空のリストを返却する.
      *
      * @param emptyListForTest   全商品データのリスト(このテストケースでは空のリストを用意する)
      * @param requestedProductID 利用者が選択した商品のID
      */
     @Test
-    public void allProductListIsEmpty() {
+    public void productListIsEmpty() {
         Product productForTest1 = new Product(1, "テスト1", "飲食料品", 100, 108, null);
         Product productForTest2 = new Product(2, "テスト2", "飲食料品", 200, 216, null);
         Product productForTest3 = new Product(3, "テスト3", "酒類", 300, 330, null);
